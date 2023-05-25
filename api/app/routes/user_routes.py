e8eddada9b08d3071f1a7895bac17df2082d6d24
Sadman Shawmik <hello@sadmansh.com>
2023-05-25 17:23:48 -0400

    C6RF-8: Abstract all user routes with /api/user/

--- api/app/routes/user_routes.py
+++ api/app/routes/user_routes.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, request, jsonify
 from app.controllers.user_controller import UserController
 
-user_routes = Blueprint('user_routes', __name__)
+user_routes = Blueprint('user_routes', __name__, url_prefix='/api/user')
 user_controller = UserController()
 
-# User registration
+# User registration route: /api/user/register
 @user_routes.route('/register', methods=['POST'])
 def register():
 	if request.method == 'POST' and request.is_json:
