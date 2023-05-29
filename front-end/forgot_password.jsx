a8f126e2246d6d8bc24d3b2e95711c86b5c41b7b
Abhinaw Siddharth Bogadhi <65751192+abhinawsiddharth@users.noreply.github.com>
2023-05-29 16:16:39 -0400

    C6RF 19: Next.js file

--- front-end/forgot_password.jsx
+++ front-end/forgot_password.jsx
@@ -4,6 +4,67 @@ const ForgotPassword = () => {
     <>
       <style jsx>{`
         
+        body {
+          background-color: #f2f2f2;
+          font-family: "Times New Roman", sans-serif;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 100vh;
+          flex-direction: column;
+        }
+
+        h2 {
+          text-align: center;
+          margin-top: 0;
+        }
+
+        form {
+          max-width: 300px;
+          margin: 0 auto;
+          padding: 20px;
+          background-color: #ffffff;
+          border-radius: 5px;
+          box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        
+        .container {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          height: 100vh;
+        }
+
+        label {
+          display: block;
+          margin-bottom: 5px;
+        }
+
+        input[type="email"],
+        input[type="date"] {
+          width: 100%;
+          padding: 8px;
+          margin-bottom: 10px;
+          border: 1px solid #ccc;
+          border-radius: 4px;
+          box-sizing: border-box;
+        }
+
+        input[type="submit"] {
+          width: 100%;
+          padding: 10px;
+          background-color: #4CAF50;
+          color: white;
+          border: none;
+          border-radius: 4px;
+          cursor: pointer;
+        }
+
+        p {
+          text-align: center;
+        }
+        
       `}</style>
 
       <div className="container">
